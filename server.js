2d43f55d6922adb6110af960a1ead3669c60aac2
Choucheneeee <chouchene.amine.etud@gmail.com>
2024-12-07 01:59:40 +0100

    Implement template and connect pages with seamless navigation

---
+++ server.js
@@ -0,0 +1,49 @@
+const express=require("express")
+const logger=require("morgan")
+const mongo=require('mongoose')
+const path=require("path")
+const app=express()
+
+
+
+app.set('view engine','ejs')
+app.use(logger("dev"))
+app.use(express.static(path.join(__dirname,'assets')))
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.render("index")
+
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+
+})
+app.get("/about",(req,res)=>{
+    res.render("about")
+
+})
+app.get("/product",(req,res)=>{
+    res.render("product")
+
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login")
+
+})
+
+app.get("/register",(req,res)=>{
+    res.render("register")
+
+})
+
+
+
+
+
+
+app.listen(3000,()=>{console.log("server is running on port 3000")}) 
